a6076d924eb4e3bd59a22ef5f4f8c06e844a1c6d
chaluvadis <surendra.chaluvadi@gmail.com>
2017-08-05 00:32:08 -0500

    add event popup added for event click

--- app.js
+++ app.js
@@ -2,52 +2,81 @@
     'use strict';
     angular.module('calendarApp', ['ui.calendar'])
         .controller('calendarController', ['$scope', '$http', 'uiCalendarConfig', function ($scope, $http, uiCalendarConfig) {
-            var date = new Date();
-            var d = date.getDate();
-            var m = date.getMonth();
-            var y = date.getFullYear();
-
-            $scope.event = {
-                eventSubject: "",
-                startDate: "",
-                endDate: "",
-                eventDescription: ""
+            $scope.events = [];
+            $scope.format = "DD/MM/YYYY HH:mm A";
+            $scope.startView = "month";
+            function clearCalendar() {
+                if (uiCalendarConfig.calendars.myCalendar != null) {
+                    uiCalendarConfig.calendars.myCalendar.fullCalendar('removeEvents');
+                    uiCalendarConfig.calendars.myCalendar.fullCalendar('unselect');
+                }
             }
 
-            $scope.events = [
-                { title: 'All Day Event', start: new Date(y, m, 1) },
-                { title: 'Long Event', start: new Date(y, m, d - 5), end: new Date(y, m, d - 2) },
-                { id: 999, title: 'Repeating Event', start: new Date(y, m, d - 3, 16, 0), allDay: false },
-                { id: 999, title: 'Repeating Event', start: new Date(y, m, d + 4, 16, 0), allDay: false },
-                { title: 'Birthday Party', start: new Date(y, m, d + 1, 19, 0), end: new Date(y, m, d + 1, 22, 30), allDay: false },
-            ];
-            $scope.eventRender = function (event, element, view) {
-                element.attr({
-                    'tooltip': event.title,
-                    'tooltip-append-to-body': true
-                });
-                $compile(element)($scope);
-            };
+            function formatDate(date) {
+                var newDate = new Date(date);
+                return newDate.getDate().toString() + "/" + newDate.getMonth().toString() + "/" + newDate.getFullYear().toString() +
+                    " " + newDate.getHours().toString() + ":" + newDate.getMinutes().toString() + " " + newDate.getHours() > 12 ? "PM" : "AM";
 
-            $scope.addEvent = function () {
-                console.log($scope.event);
-            };
+            }
 
-            $scope.alertOnEventClick = function (date, jsEvent, view) {
-                $scope.alertMessage = (date.title + ' was clicked ');
-            };
 
-            $scope.uiConfig = {
-                calendar: {
-                    header: {
-                        left: 'title',
-                        center: '',
-                        right: 'today prev,next'
-                    },
-                    eventClick: $scope.alertOnEventClick,
-                    eventRender: $scope.eventRender
+            /* Change View */
+            $scope.changeView = function (view, calendar) {
+                uiCalendarConfig.calendars[calendar].fullCalendar('changeView', view);
+            };
+            /* Change View */
+            $scope.renderCalender = function (calendar) {
+                if (uiCalendarConfig.calendars[calendar]) {
+                    uiCalendarConfig.calendars[calendar].fullCalendar('render');
                 }
             };
+
+            function getCalendarEvents() {
+                clearCalendar();
+                $scope.events.slice(0, $scope.events.length);
+                $http.get("http://localhost:3000/events")
+                    .then(function (result) {
+                        var data = result.data;
+                        console.log(data);
+                        angular.forEach(data, (item) => {
+                            $scope.events.push(item);
+                        })
+                    }, function (err) {
+                        console.error(err);
+                    })
+            }
+            $scope.showModal = function (event) {
+                event.start = formatDate(event.start);
+                $scope.event = event;
+                $('#myModal').modal('toggle');
+            }
+            $scope.uiConfig = {
+                calendar: {
+                    height: 600,
+                    displayEventTime: true,
+                    selectable: true,
+                    selectHelper: true,
+                    header: {
+                        left: 'month agendaWeek agendaDay',
+                        center: 'title',
+                        right: 'today prev,next'
+                    },
+                    timeFormat: {
+                        month: ' ', // for hide on month view
+                        agenda: 'h:mm t'
+                    },
+                    eventClick: function (event) {
+                        console.log(event);
+                        $scope.showModal(event);
+                    },
+                    eventAfterAllRender: function () {
+                        if ($scope.events.length > 0) {
+                            uiCalendarConfig.calendars.myCalendar.fullCalendar('gotoDate', $scope.events[0].start);
+                        }
+                    }
+                }
+            }
+            getCalendarEvents();
             $scope.eventSources = [$scope.events];
         }]);
 })()
