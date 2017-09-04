0ef33809caa04d2ccce6e6c3040736392fd9ea54
chaluvadis <surendra.chaluvadi@gmail.com>
2017-09-04 11:13:38 -0500

    impmentation is replaced with controller as

--- app.js
+++ app.js
@@ -1,82 +1,73 @@
 (function () {
     'use strict';
-    angular.module('calendarApp', ['ui.calendar'])
-        .controller('calendarController', ['$scope', '$http', 'uiCalendarConfig', function ($scope, $http, uiCalendarConfig) {
-            $scope.events = [];
-            $scope.format = "DD/MM/YYYY HH:mm A";
-            $scope.startView = "month";
-            function clearCalendar() {
-                if (uiCalendarConfig.calendars.myCalendar != null) {
-                    uiCalendarConfig.calendars.myCalendar.fullCalendar('removeEvents');
-                    uiCalendarConfig.calendars.myCalendar.fullCalendar('unselect');
-                }
+    angular.module('calendarApp', ['ui.calendar']).controller('calendarController', calendarController);
+    calendarController.$inject = ['$scope', '$http', 'uiCalendarConfig'];
+    function calendarController($scope, $http, uiCalendarConfig) {
+        var vm = this;
+        vm.events = [];
+        vm.format = "DD/MM/YYYY HH:mm A";
+        function clearCalendar() {
+            if (uiCalendarConfig.calendars.myCalendar != null) {
+                uiCalendarConfig.calendars.myCalendar.fullCalendar('removeEvents');
+                uiCalendarConfig.calendars.myCalendar.fullCalendar('unselect');
             }
+        }
+        function formatDate(date) {
+            var newDate = new Date(date);
+            return newDate.getDate().toString() + "/" + newDate.getMonth().toString() + "/" + newDate.getFullYear().toString() +
+                " " + newDate.getHours().toString() + ":" + newDate.getMinutes().toString() + " " + newDate.getHours() > 12 ? "PM" : "AM";
 
-            function formatDate(date) {
-                var newDate = new Date(date);
-                return newDate.getDate().toString() + "/" + newDate.getMonth().toString() + "/" + newDate.getFullYear().toString() +
-                    " " + newDate.getHours().toString() + ":" + newDate.getMinutes().toString() + " " + newDate.getHours() > 12 ? "PM" : "AM";
-
+        }
+        vm.changeView = function (view, calendar) {
+            uiCalendarConfig.calendars[calendar].fullCalendar('changeView', view);
+        }
+        vm.renderCalender = function (calendar) {
+            if (uiCalendarConfig.calendars[calendar]) {
+                uiCalendarConfig.calendars[calendar].fullCalendar('render');
             }
-
-
-            /* Change View */
-            $scope.changeView = function (view, calendar) {
-                uiCalendarConfig.calendars[calendar].fullCalendar('changeView', view);
-            };
-            /* Change View */
-            $scope.renderCalender = function (calendar) {
-                if (uiCalendarConfig.calendars[calendar]) {
-                    uiCalendarConfig.calendars[calendar].fullCalendar('render');
-                }
-            };
-
-            function getCalendarEvents() {
-                clearCalendar();
-                $scope.events.slice(0, $scope.events.length);
-                $http.get("http://localhost:3000/events")
-                    .then(function (result) {
-                        var data = result.data;
-                        console.log(data);
-                        angular.forEach(data, (item) => {
-                            $scope.events.push(item);
-                        })
-                    }, function (err) {
-                        console.error(err);
+        }
+        function getCalendarEvents() {
+            clearCalendar();
+            vm.events.slice(0, vm.events.length);
+            $http.get("http://localhost:3000/events")
+                .then(function (result) {
+                    var data = result.data;
+                    angular.forEach(data, (item) => {
+                        vm.events.push(item);
                     })
+                }, function (err) {
+                    console.error(err);
+                })
+        }
+        vm.showModal = function (event) {
+            event.start = formatDate(event.start);
+            vm.event = event;
+            $('#myModal').modal('toggle');
+        }
+
+        function onEventClick(event, jsEvent, view) {
+            console.log(event);
+        }
+        function onDayClick(event, jsEvent, view) {
+            console.log(event);
+        }
+
+        vm.uiConfig = {
+            calendar: {
+                height: 600,
+                displayEventTime: true,
+                selectable: true,
+                selectHelper: true,
+                header: {
+                    left: 'month agendaWeek agendaDay',
+                    center: 'title',
+                    right: 'today prev,next'
+                },
+                eventClick: onEventClick,
+                dayClick: onDayClick
             }
-            $scope.showModal = function (event) {
-                event.start = formatDate(event.start);
-                $scope.event = event;
-                $('#myModal').modal('toggle');
-            }
-            $scope.uiConfig = {
-                calendar: {
-                    height: 600,
-                    displayEventTime: true,
-                    selectable: true,
-                    selectHelper: true,
-                    header: {
-                        left: 'month agendaWeek agendaDay',
-                        center: 'title',
-                        right: 'today prev,next'
-                    },
-                    timeFormat: {
-                        month: ' ', // for hide on month view
-                        agenda: 'h:mm t'
-                    },
-                    eventClick: function (event) {
-                        console.log(event);
-                        $scope.showModal(event);
-                    },
-                    eventAfterAllRender: function () {
-                        if ($scope.events.length > 0) {
-                            uiCalendarConfig.calendars.myCalendar.fullCalendar('gotoDate', $scope.events[0].start);
-                        }
-                    }
-                }
-            }
-            getCalendarEvents();
-            $scope.eventSources = [$scope.events];
-        }]);
+        }
+        getCalendarEvents();
+        vm.eventSources = [vm.events];
+    }
 })()
